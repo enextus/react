33f794d56b4750595088d0e60e826c0ed9c56261
enextus <enextus@gmail.com>
2019-06-11 16:36:33 +0300

    User func

--- start-react/src/User.jsx
+++ start-react/src/User.jsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
 // export default function User(props) {
-
 //     console.log(props);
-
 //     return (
 //         <div>
 //             <h3>My Name is {props.userData.name}</h3>
@@ -22,20 +20,24 @@ export default function User(props) {
 
     // const { childOne = 'Anonymous', childTwo = 'Anonymous', childThree = 'Anonymous', } = children;
 
+    const arr = [];
+
     for (const [key, value] of Object.entries(children)) {
-        console.log(key, value);
+        arr.push(key, value);
     }
-    
+
     return (
-        <div>
+        <div id='main'>
             <h3>My name: {name}</h3>
             <h3>My nickname: {nickname}</h3>
             <h3>My surname: {surname}</h3>
             <h3>My age: {age}</h3>
             <h3>My rating: {rating}</h3>
-            {/* <h3>My first child's name: {childOne}</h3>
-            <h3>My second child's name: {childTwo}</h3>
-            <h3>My third child's name: {childThree}</h3> */}
+            { arr.map(function (n) {
+                return ([
+                <h3>{n}</h3>
+                ]);
+            }) }
         </div>
     )
 }
