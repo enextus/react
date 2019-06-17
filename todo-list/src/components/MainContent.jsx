f0a3773b536e7dc1476c36ab5a9b8035c943287a
enextus <enextus@gmail.com>
2019-06-17 21:40:56 +0300

    some rev

--- todo-list/src/components/MainContent.jsx
+++ todo-list/src/components/MainContent.jsx
@@ -4,14 +4,27 @@ import './MainContent.css';
 
 function MainContent() {
 
-    const firstName = "Bob"
-    const lastName = "Ziroll"
+    const date = new Date()
+    const hours = date.getHours()
+    let timeOfDay
+    const styles = {
+      fontSize: 30
+    }
+    
+    if (hours < 12) {
+      timeOfDay = "morning"
+      styles.color = "#04756F"
+    } else if (hours >= 12 && hours < 17) {
+      timeOfDay = "afternoon"
+      styles.color = "#2E0927"
+    } else {
+      timeOfDay = "night"
+      styles.color = "#D90000"
+    }
 
     return (
         <main className="main">
-            ▍main element
-            <br />
-            {firstName + " " + lastName}
+            ▍main element<h1 style={styles}>Good {timeOfDay}!</h1>
         </main>
     )
 }
