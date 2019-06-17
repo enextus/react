2758f982fb14d040a987b44d1b31086081385dcf
enextus <enextus@gmail.com>
2019-06-17 13:03:22 +0300

    some rev

--- todo-list/src/components/MainContent.jsx
+++ todo-list/src/components/MainContent.jsx
@@ -3,9 +3,15 @@ import React from "react"
 import './MainContent.css';
 
 function MainContent() {
+
+    const firstName = "Bob"
+    const lastName = "Ziroll"
+
     return (
         <main className="main">
             ▍main element
+            <br />
+            {firstName + " " + lastName}
         </main>
     )
 }
